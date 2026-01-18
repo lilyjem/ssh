7c049404e3221f48f40b2ca11768613047bd5a48
jem <abcjem@gmail.com>
2026-01-18 13:13:16 +0800

    chore: remove test folder from git tracking, keep locally

--- test/constants.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect } from "vitest";
-import { CHARACTER_LIMIT } from "../src/constants";
-
-describe("constants", () => {
-  it("exports CHARACTER_LIMIT", () => {
-    expect(CHARACTER_LIMIT).toBeGreaterThan(0);
-  });
-});
